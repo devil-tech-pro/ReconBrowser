76babf72e2e2e942d8cfd9d1d59d233169baf3f8
Devil <kumarsumit151020@gmail.com>
2026-03-26 22:15:38 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,12 +2,8 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("api",{
 
-scanSubdomains:(domain)=>ipcRenderer.invoke("scan-subdomains",domain),
+scanSite:(domain)=>ipcRenderer.invoke("scan-site",domain),
 
-scanPorts:(host)=>ipcRenderer.invoke("scan-ports",host),
-
-findIP:(domain)=>ipcRenderer.invoke("find-ip",domain),
-
-onHeaders:(callback)=>ipcRenderer.on("headers-data",(e,data)=>callback(data))
+onHeaders:(callback)=>ipcRenderer.on("headers",(e,data)=>callback(data))
 
 });
